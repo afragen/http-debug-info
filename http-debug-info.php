e10cd81fad0a53bb4d7998936b1f249a7b32684b
Andy Fragen <andy@thefragens.com>
2017-05-21 10:13:31 -0700

    create thickbox of current filters

--- http-debug-info.php
+++ http-debug-info.php
@@ -7,6 +7,7 @@
  * Author: Andy Fragen
  * License: MIT
  * GitHub Plugin URI: https://github.com/afragen/http-debug-info
+ * GitHub Branch: currrent-filters
  */
 
 add_action( 'http_api_debug', function( $response, $type, $class, $args, $url ) {
@@ -36,3 +37,29 @@ add_action( 'http_api_debug', function( $response, $type, $class, $args, $url ) 
 	<br>
 	<?php
 }, 15, 5 );
+
+add_action( 'admin_init', function() {
+	$needle = 'lock';
+	foreach ( array_keys( $GLOBALS['wp_filter'] ) as $filter ) {
+		if ( false !== strpos( $filter, $needle ) ) {
+			$called_filters[] = $filter;
+		}
+	}
+	if ( ! empty( $called_filters ) ) {
+		add_thickbox();
+
+		?>
+		<div id="current-filter-id-<?php echo md5( $needle ); ?>" class="thickbox" style="display:none;">
+			<p>
+				<strong>WordPress Called Filters</strong>
+				<?php
+				print_r( '<pre>' . 'Called Filters' . "\n" . json_encode( $called_filters, 128 | 64 ) . '</pre>' );
+
+				?>
+			</p>
+		</div>
+		<a href="#TB_inline?width=600&height=550&inlineId=current-filter-id-<?php echo md5( $needle ); ?>" class="thickbox" style="margin-left:30em;clear:both;">View Current Filters!</a>
+		<br>
+		<?php
+	}
+} );

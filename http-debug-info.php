152d1c4d97284e2b805195d7daa846950ed1d62b
Andy Fragen <andy@thefragens.com>
2017-05-21 11:09:32 -0700

    remove http_api_debug display

--- http-debug-info.php
+++ http-debug-info.php
@@ -10,33 +10,6 @@
  * GitHub Branch: current-filters
  */
 
-add_action( 'http_api_debug', function( $response, $type, $class, $args, $url ) {
-	$skips = array( 'wordpress.org', $_SERVER['HTTP_HOST'] );
-	foreach ( $skips as $skip ) {
-		if ( stripos( $url, $skip ) ) {
-			return;
-		}
-	}
-	add_thickbox();
-
-	?>
-	<div id="http-debug-id-<?php echo md5( $url ); ?>" class="thickbox" style="display:none;">
-		<p>
-			<strong>HTTP Debug Information</strong>
-			<?php
-			print_r( '<pre>' . 'Request Type:' . "\n" . json_encode( $type, 128 | 64 ) . '</pre>' );
-			print_r( '<pre>' . 'Request Class:' . "\n" . json_encode( $class, 128 | 64 ) . '</pre>' );
-			print_r( '<pre>' . 'Request URL:' . "\n" . json_encode( $url, 128 | 64 ) . '</pre>' );
-			print_r( '<pre>' . 'Request Args:' . "\n" . json_encode( $args, 128 | 64 ) . '</pre>' );
-			print_r( '<pre>' . 'Request Response:' . "\n" . json_encode( $response, 128 | 64 ) . '</pre>' );
-			?>
-		</p>
-	</div>
-
-	<a href="#TB_inline?width=600&height=550&inlineId=http-debug-id-<?php echo md5( $url ); ?>" class="thickbox" style="margin-left:30em;clear:both;">View HTTP Debug content!</a>
-	<br>
-	<?php
-}, 15, 5 );
 
 add_action( 'admin_init', function() {
 	$needle = 'lock';
